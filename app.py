b2bb53a6a0ae4734247a12faefa8be51230bf59d
MINA000000 <minanasserwanaszero@gmail.com>
2024-09-21 01:10:47 +0300

    test this app

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-# app.py
-from flask import Flask, request, jsonify
-import pickle
-import numpy as np
+from flask import Flask
 
 app = Flask(__name__)
 
-# Load the saved model
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-# Define a route for predictions
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()  # Get JSON data from the request body
-    features = np.array(data['features'])  # Extract features from JSON
-    prediction = model.predict([features])  # Use the model to predict
-    return jsonify({'prediction': int(prediction[0])})  # Return the prediction as JSON
+@app.route('/')
+def home():
+    return "Hello, World! mina nasser"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
